105206b2f941697c57bc9f42d54f09f00c910c18
David Darrell <david.darrell@gmail.com>
2017-08-05 09:49:30 +0800

    Test OkHttp

--- app/src/main/java/com/veggedup/veggedup/MainActivity.java
+++ app/src/main/java/com/veggedup/veggedup/MainActivity.java
@@ -1,7 +1,16 @@
 package com.veggedup.veggedup;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public class TestOkHttpTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url("http://veggedup.com")
+                    .build();
+            String results = null;
+            try {
+                Response response = client.newCall(request).execute();
+                results = response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void onPostExecute(String results) {
+            Log.d("Test", results);
+        }
+    }
+
+    public void onClickTestOkHttp(View view) throws IOException {
+        new TestOkHttpTask().execute("http://veggedup.com");
     }
 }
