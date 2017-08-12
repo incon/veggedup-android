dffd6faf701eeb5b0b183aeeeda0bab3586f5973
David Darrell <david.darrell@gmail.com>
2017-08-12 20:51:42 +0800

    Get the recipe details from the database

--- app/src/main/java/com/veggedup/veggedup/RecipeDetailActivity.java
+++ app/src/main/java/com/veggedup/veggedup/RecipeDetailActivity.java
@@ -1,6 +1,8 @@
 package com.veggedup.veggedup;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,17 +11,30 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 
+import com.veggedup.veggedup.data.VeggedupContract;
+import com.veggedup.veggedup.data.VeggedupDbHelper;
+
 public class RecipeDetailActivity extends AppCompatActivity {
+
+    private SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        VeggedupDbHelper dbHelper = new VeggedupDbHelper(this);
+        mDb = dbHelper.getReadableDatabase();
+
+        Cursor recipe = null;
         Intent intent = getIntent();
         if (intent.hasExtra("RECIPE_ID")){
             int recipeId = intent.getIntExtra("RECIPE_ID", 0);
+            recipe = getRecipe(recipeId);
+            recipe.moveToFirst();
             Log.v("RECIPE_ID", String.valueOf(recipeId));
         }
+        
+        int recipeId = recipe.getInt(recipe.getColumnIndex(VeggedupContract.Recipe.COLUMN_RECIPE_ID));
 
         setContentView(R.layout.activity_recipe_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -34,6 +49,18 @@ public class RecipeDetailActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private Cursor getRecipe(int recipeId) {
+        return mDb.query(
+                VeggedupContract.Recipe.TABLE_NAME,
+                null,
+                VeggedupContract.Recipe.COLUMN_RECIPE_ID + " = " + recipeId,
+                null,
+                null,
+                null,
+                VeggedupContract.Recipe.COLUMN_RECIPE_ID
+        );
     }
 
 }
